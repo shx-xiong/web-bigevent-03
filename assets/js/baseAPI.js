7b5fe25e5225dc834ff8b31665a4642e00f30eee
xiongshuxiang <shuxiang_x@163com>
2020-10-27 21:13:20 +0800

    完成注册和登录功能

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,10 @@
+var baseURL = 'http://ajax.frontend.itheima.net'
+$.ajaxPrefilter(function (options) {
+    options.url = baseURL + options.url
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token')
+        }
+    }
+
+})
