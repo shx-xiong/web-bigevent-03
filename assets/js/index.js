98d2fde2b474577b27a9a57c6c39c0e103c111b1
xiongshuxiang <shuxiang_x@163com>
2020-10-28 10:08:02 +0800

    完成首页布局和获取用户信息功能

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+$(function () {
+    // 获取用户信息
+    getUserInfo()
+})
+
+//封装获取用户信息函数
+function getUserInfo() {
+    $.ajax({
+        url: '/my/userinfo',
+        success: function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            renderAvatar(res.data)
+        }
+    })
+}
+
+//封装渲染用户头像函数
+
+function renderAvatar(user) {
+    var name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').show().attr('src', user.user_pic)
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        var text = name[0].toUpperCase()
+        $('.text-avatar').show().html(text)
+    }
+}
